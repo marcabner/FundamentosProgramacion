e9cb834da90c348c5a244920d3df4a23f2d13523
yair abner <marcabner@gmail.com>
2023-12-16 15:40:53 -0600

    Edita el archivo  y guarda para recargar .src/App.js

--- fundamentos_programacion_01/src/App.js
+++ fundamentos_programacion_01/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edita el archivo  <code>src/App.js</code> y guarda para recargar.3
         </p>
         <a
           className="App-link"
